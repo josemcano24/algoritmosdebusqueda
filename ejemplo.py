49251c74e4a507454261d1644abfc955d1d9846c
Josemaria Cano Cavazos <136494102+josemcano24@users.noreply.github.com>
2025-10-24 11:47:35 -0600

    Add files via upload

---
+++ ejemplo.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Sumadora de dos numeros")
+
+numero1 = st.number_input("ingresa el primer numero", value=0)
+
+numero2 = st.number_input("ingresa el segundo numero", value=0)
+
+suma = numero1 + numero2
+
+st.write(f"la suma de {numero1} + {numero2} es: {suma}")
